2b6d29df7c41d0782a6db01d838e41f575129a30
RoboCopGay <vinicios.sousa909@gmail.com>
2020-04-24 09:52:23 -0300

    adding the get_pix_map function and renaming other function

--- image.py
+++ image.py
@@ -1,8 +1,14 @@
 from analizer import media
-from PIL import Image
+from PIL import Image, PyAccess
 
-def get_normalized(img):
+def get_gray_shade(img):
     img_pix_map = img.load()
     for x in range(img.width):
         for y in range(img.height):
             yield x, y, media( img_pix_map[x, y] )
+
+
+def get_pix_map(new_pix_map):
+    for x in range(len(new_pix_map)):
+        for y in range(len(new_pix_map[0])):
+            yield x, y, tuple([new_pix_map[x][y] for i in range(3)])

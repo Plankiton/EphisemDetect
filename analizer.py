8ab1fd3182ce35af61a58847be2bf3a886b88083
RoboCopGay <vinicios.sousa909@gmail.com>
2020-05-04 09:02:12 -0300

    adding the data preparer

--- analizer.py
+++ analizer.py
@@ -47,6 +47,51 @@ def threshold(img: Image):
 
     return new_img
 
+if '-t' in argv:
+    from json import dumps as json
+    from os import listdir
+    slices = []
+    dir_list = listdir('slices')
+    data = open('slices.json', 'a')
+    data.write('{')
+    i = 0
+    for slice in dir_list:
+        name = slice[:slice.index('_')]
+        if '.tiff' in slice and name not in slices:
+            slices.append(name)
+            print(name)
+
+            top = f'{name}_top.tiff'
+            top = asarray(threshold(Image.open(
+                f'slices/{top}',
+            ))).tolist() if top in dir_list else None
+
+            middle = f'{name}_middle.tiff'
+            middle = asarray(threshold(Image.open(
+                f'slices/{middle}',
+            ))).tolist() if middle in dir_list else None
+
+            bottom = f'{name}_bottom.tiff'
+            bottom = asarray(threshold(Image.open(
+                f'slices/{bottom}',
+            ))).tolist() if bottom in dir_list else None
+
+            dic = {name: {
+                'top': top,
+                'middle': middle,
+                'bottom': bottom
+            }}
+
+            data.write(json(dic)+',\n' if i < len(dir_list)-1 \
+                       else '\n')
+
+            i += sum([0 if not j else 1 for j in (
+                top, middle, bottom
+            )])
+    data.write('}')
+    data.close()
+    print('slices threshold saveds')
+    exit()
 
 img = Image.open(argv[1])
 img = threshold(img)

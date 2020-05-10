9294ead1cf5cffae2ef59bedc0f8398971baa5f4
RoboCopGay <vinicios.sousa909@gmail.com>
2020-05-09 22:43:32 -0300

    first usable version

--- analizer.py
+++ analizer.py
@@ -3,7 +3,34 @@ from numpy import asarray
 from PIL import Image
 from sys import argv
 
+def compress_map(img_map:list):
+    c = 0
+    new_map = []
+    w, h = len(img_map), len(img_map[0])
+    size_range = int((w + h) / 2 * 0.006)
+    for x in range(0, w, size_range):
+        new_map.append([])
+        for y in range(0, w, size_range):
+            xend = x+size_range
+            xend = xend if xend < w else w
+            yend = y+size_range
+            yend = yend if yend < w else w
+
+            pix_range = [[
+                img_map[i][j] for j in range(y, yend)
+            ] for i in range(x, xend)]
+
+            n = 0
+            for i in pix_range:
+                for j in i:
+                    n += 1 if j == (0,0,0) else 0
+            pixel = n*100/len(i) > 50
+            new_map[c].append(pixel)
+        c += 1
+    return new_map
+
 def prepar_data():
+    from os import listdir
     ignore = argv[argv.index('-i')+1:] if '-i' in argv else []
 
     slices = {}
@@ -12,37 +39,34 @@ def prepar_data():
         while i in dir_list:
             dir_list.remove(i)
 
-    data = open('slices.json', 'a')
-
-    print(':: Creating data')
     for slice in dir_list:
         name = slice[:slice.index('_')]
+        print(f'  -> {name}')
         if '.tiff' in slice and name not in slices:
-            print('   -> creating ',name)
 
             top = f'{name}_top.tiff'
-            top = asarray(threshold(Image.open(
+            top = None
+            '''asarray(threshold(Image.open(
                 f'slices/{top}',
-            ))).tolist() if top in dir_list else None
+            ))).tolist() if top in dir_list else None'''
 
             middle = f'{name}_middle.tiff'
-            middle = asarray(threshold(Image.open(
+            middle = compress_map(threshold(Image.open(
                 f'slices/{middle}',
-            ))).tolist() if middle in dir_list else None
+            ))) if middle in dir_list else None
 
             bottom = f'{name}_bottom.tiff'
-            bottom = asarray(threshold(Image.open(
+            bottom = None
+            '''asarray(threshold(Image.open(
                 f'slices/{bottom}',
-            ))).tolist() if bottom in dir_list else None
+            ))).tolist() if bottom in dir_list else None'''
 
             slices[name] = {
                 'top': top,
                 'middle': middle,
                 'bottom': bottom
             }
-    data.write(json(slices))
-    data.close()
-    print(':: All is done!')
+    return slices
 
 def _threshold(img_ar: asarray):
     ar_backup = img_ar
@@ -70,32 +94,47 @@ def _threshold(img_ar: asarray):
                 new_ar[x].append(tuple([255 for i in range(3)]))
             else:
                 new_ar[x].append(tuple([0 for i in range(3)]))
+
     return new_ar
+
+def to_img(img_ar: asarray):
+    new_img = Image.new(mode='RGBA',
+                        size = (len(img_ar[0]), len(img_ar)))
+    pix_map = new_img.load()
+
+    for x in range(len(img_ar)):
+        for y in range(len(img_ar[0])):
+            pix_map[x, y] = img_ar[x][y]
+
+    return new_img
 
 
 def threshold(img: Image):
     if img.mode == 'I':
         img = img.convert('RGBA')
     img_ar = asarray(img)
-    new_img = Image.new(mode='RGBA',
-                        size = (img.width, img.height))
-    pix_map = new_img.load()
+    return _threshold(img_ar)
 
-    threshold_ar = _threshold(img_ar)
-    for x in range(len(img_ar)):
-        for y in range(len(img_ar[0])):
-            pix_map[x, y] = threshold_ar[x][y]
 
-    return new_img
 
-if '-t' in argv:
-    from json import dumps as json
-    from os import listdir
+if __name__ == '__main__':
+    print(':: Preparing data')
+    data = prepar_data()
 
-    prepar_data()
-    exit()
-from json import load as json
-data = json(open('slices.json'))
+    print(':: Preparing image inputed')
+    img = Image.open(argv[1])
+    img_map = compress_map(threshold(img))
 
-img = Image.open(argv[1])
-img = threshold(img)
+    percentages = []
+    print('\n\n:: Starting image compair')
+    for img in data:
+        value = 0
+        img = data[img]['middle']
+        if not img:continue
+        for x in range(len(img)):
+            for y in range(len(img[0])):
+                if img[x][y] == img_map[x][y]:
+                    value += 1
+        percentages.append(value*100/(len(img[0])*len(img)))
+
+    print(f'\n\nEphisem level = {sum(percentages)/len(percentages)}%')

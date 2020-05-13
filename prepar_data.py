b072adbb60dffee4b8f47ad25a3c83250857a406
RoboCopGay <vinicios.sousa909@gmail.com>
2020-05-13 10:58:50 -0300

    fixing a data extract error

--- prepar_data.py
+++ prepar_data.py
@@ -1,8 +1,9 @@
 from ephisem_detect import prepar_data
 from json import dumps
 
+data = {}
 for slice_type in ['middle', 'bottom', 'top']:
     print(f':: Preparing {slice_type}')
-    data = prepar_data(slice_type = slice_type, slices_dir = 'static/slices')
+    data[slice_type] = prepar_data(slice_type = slice_type, slices_dir = 'static/slices')
 with open('static/data.json', 'w') as file:
     file.write(dumps(data))
